f54b1951d0ee1bfae5283389470f4b408597fc76
bala-amypo <bala@amypo.in>
2025-12-19 08:16:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimeStampEntity.java
+++ src/main/java/com/example/demo/entity/TimeStampEntity.java
@@ -11,10 +11,29 @@ private LocalDateTime updateAt;
     private Long id;
     private String name;
     private String email;
-    private String password;
-    private Date created;
+
 
 }
 
+package com.example.demo.entity;
 
+import jakarta.validation.constraints.*;
+import jakata.persistence.*;
+import lombok.*;
+import java.util.*;
+@Entity
+@Data
+@Getter
+@Setter
+@AllAgrsConstructor
+@NoAgrsConstructor
+public class TimeSampEntity{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY);
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
 

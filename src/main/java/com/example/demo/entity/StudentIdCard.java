f8b770add7ca6e45d5b0e6ec6ae31a0a24fd00fc
bala-amypo <bala@amypo.in>
2025-12-19 10:01:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentIdCard.java
+++ src/main/java/com/example/demo/entity/StudentIdCard.java
@@ -3,6 +3,10 @@ package com.example.demo.entity;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.JoinColumn;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentIdCard{
  @OneToOne
 @JoinColumn

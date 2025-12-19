722413a3e90032214569eab36dc500bedad9ae63
bala-amypo <bala@amypo.in>
2025-12-19 08:13:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimeStampEntity.java
+++ src/main/java/com/example/demo/entity/TimeStampEntity.java
@@ -1,15 +1,19 @@
 package com.example.demo.entity;
 
 
+import java.time.localDateTime;
+
 public class TimeStampEntity{
 
+private LocalDateTime createAt;
+private LocalDateTime updateAt;
 
-    private Integer id;
+    private Long id;
     private String name;
     private String email;
     private String password;
     private Date created;
-    
+
 }
 
 
--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -16,7 +16,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
-import java.time.localDateTime;
+
 
 @Entity
 @Data
@@ -38,8 +38,7 @@ public class ValidationEntity{
     @Positive(message="Age is be a positive number")
     private int age;
 
-private LocalDateTime createAt;
-private LocalDateTime updateAt;
+
 }
 
 

9b41da13ceee323999e2b8cd663f3da8f96262af
bala-amypo <bala@amypo.in>
2025-12-19 09:58:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentDetails.java
+++ src/main/java/com/example/demo/entity/StudentDetails.java
@@ -1,11 +1,6 @@
 package com.example.demo.entity;
 
-import jaka
 
-
-
-@OneToOne
-@JoinColumn
 public class StudentDetails{
     
 }
--- src/main/java/com/example/demo/entity/StudentIdCard.java
+++ src/main/java/com/example/demo/entity/StudentIdCard.java
@@ -1,7 +1,12 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
 
 public class StudentIdCard{
-    
+ @OneToOne
+@JoinColumn
+private StudentDetails stu;
+
 }
 

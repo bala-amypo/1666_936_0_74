8ded68f5218007af447ae91ea5e601b358b8218f
bala-amypo <bala@amypo.in>
2025-12-19 05:11:19 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
@@ -17,7 +17,7 @@ public ValidationEntity postData(ValidationEntity stu){
     return student.save(stu);
 }
 @Override
-public ValidationEntity getStudentById(Integer id){
+public ValidationEntity getStudentById(Long id){
     return student.findById(id).orElseThrow(()->new ValidationException("Invalid Id"+id));
 }
 }

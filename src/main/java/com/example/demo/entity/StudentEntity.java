72c39addd63d978751dc7757dd0e2adcc85f05bc
bala-amypo <bala@amypo.in>
2025-12-19 06:27:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -19,43 +19,4 @@ public class StudentEntity{
     private String password;
     private Date created;
 
-public Date getCreated(){
-    return created;
-}
-public void setCreated(Date created){
-    this.created=created;
-}
-public void setId(Integer id){
-    this.id=id;
-}
-public String getName(){
-    return name;
-}
-public void setName(){
-    this.name=name;
-}
-public String getEmail(){
-    return email;
-}
-public void setEmail(){
-    this.email=email;
-}
-public String getPassword(){
-    return password;
-}
-public void setPasswod(String password){
-    this.password=password;
-}
-
-public StudentEntity(Integer id,String name,String email,String password,Date created){
-    this.id=id;
-    this.name=name;
-    this.email=email;
-    this.password=password;
-    this.created=created;
-}
-public StudentEntity(){
-
-}
-
 }
--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -29,48 +29,6 @@ public class ValidationEntity{
     @Positive(message="Age is be a positive number")
     private int age;
 
-public Long getId(){
-    return id;
-}
-public void setId(Long id){
-    this.id=id;
-}
-public String getUsername(){
-    return username;
-}
-public void setUsername(){
-    this.username=username;
-}
-public String getEmail(){
-    return email;
-}
-public void setEmail(){
-    this.email=email;
-}
-public String getPassword(){
-    return password;
-}
-public void setPasswod(String password){
-    this.password=password;
-}
-public int getAge(){
-    return age;
-}
-public void setAge(){
-    this.age=age;
-}
-public ValidationEntity(Long id,
-    @NotNull @Size(min=2,max=0,message="must be 2 to 10 character") String username,
-    @Email(message="Email is not valid") String email,
-    @Size(min=2,max=0,message="must be 2 to 10 character")
-    @NotNull(message="password is mandatory") String password,@Max(30)
-    @Positive(message="Age is be a positive number") int age){
-        this.id=id;
-        this.username=username;
-        this.email=email;
-        this.password=password;
-        this.age=age;
-    }
 
 }
 

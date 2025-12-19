f13e48ad7c003c168f39a1620e8477d6217e606f
bala-amypo <bala@amypo.in>
2025-12-19 07:58:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimeStampEntity.java
+++ src/main/java/com/example/demo/entity/TimeStampEntity.java
@@ -3,6 +3,13 @@ package com.example.demo.entity;
 
 public class TimeStampEntity{
 
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+    private Date created;
+    
 }
 
 
